78108801c5cf3f6401d34ec7b7ab831d1724c16a
kawasaki-kazuki <kawasaki.kazuki@layered.inc>
2024-08-07 17:27:33 +0900

    models>user.rbに入力制限を設ける

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+  #データ保存時に小文字にする
+  before_save { self.email = self.email.downcase } 
+  #入力必須かつ50文字以上入力要求
+  validates :name,
+    presence: true,
+    length: { maximum: 50 } 
+  #アドレスの正規表現パターンを定義
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i 
+  validates :email,
+    presence: true, 
+    length: { maximum: 255 },
+    format: { with: VALID_EMAIL_REGEX },
+    uniqueness: { case_sensitive: false } #アドレスは
+  has_secure_password
 end
