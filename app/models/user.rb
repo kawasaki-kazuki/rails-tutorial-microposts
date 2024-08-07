998abd401a5122c4eb3276d36d6973a983eb75d7
kawasaki-kazuki <kawasaki.kazuki@layered.inc>
2024-08-07 17:44:26 +0900

    users_controller.rbにUserのcreateメソッドを追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,12 @@ class User < ApplicationRecord
     length: { maximum: 50 } 
   #アドレスの正規表現パターンを定義
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i 
+  #アドレスのルール：
+  ##入力必須で255文字上限、正規表現とマッチ、一意となるように
   validates :email,
     presence: true, 
     length: { maximum: 255 },
     format: { with: VALID_EMAIL_REGEX },
-    uniqueness: { case_sensitive: false } #アドレスは
+    uniqueness: { case_sensitive: false } 
   has_secure_password
 end
