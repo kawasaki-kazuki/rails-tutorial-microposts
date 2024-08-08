3f351e1cc2cad27fc2517d2c66be65f6a70b8d42
kawasaki-kazuki <kawasaki.kazuki@layered.inc>
2024-08-09 01:57:33 +0900

    add sign sign up and user detail modified:   app/assets/stylesheets/custom.scss modified:   app/controllers/users_controller.rb new file:   app/helpers/users_helper.rb modified:   app/views/layouts/application.html.erb new file:   app/views/users/show.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,14 @@
-class UsersController < ApplicationController
+class UsersController < ApplicationController  
+  def show
+    @user = User.find(params[:id])
+  end
   def new
     @user = User.new
   end
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Sample App!" # 成功メッセージをフラッシュする
+      flash[:success] = "Welcome to the MICROPOSTS!" # 成功メッセージをフラッシュする
       redirect_to @user # ユーザー詳細ページにリダイレクト
     else
       # バリデーションエラー時の処理
