41a4c261970076bef8aa4a9a2328b08afc829337
kawasaki-kazuki <kawasaki.kazuki@layered.inc>
2024-08-09 11:18:01 +0900

    login機能を実装 modified:   app/controllers/application_controller.rb new file:   app/controllers/sessions_controller.rb new file:   app/helpers/sessions_helper.rb modified:   app/views/layouts/_header.html.erb new file:   app/views/sessions/new.html.erb modified:   app/views/users/new.html.erb modified:   config/routes.rb

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,13 @@
+module SessionsHelper
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+  def logged_in?
+    !!current_user
+  end
+  def store_location
+    if request.get?
+      session[:forwarding_url] = request.url
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  # get 'static_pages/home'
   root to: 'static_pages#home'
   get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
   resources :users
 
