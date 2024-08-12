c4ef4d6cd65ef89cbcdbb6a1d718c61d4760f71d
kawasaki-kazuki <kawasaki.kazuki@layered.inc>
2024-08-13 01:57:52 +0900

    ユーザープロフィールの編集機能を実装 - fix:Rubocopをformatterとして徐々に機能させている

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,45 @@
-class UsersController < ApplicationController  
-  def show
-    @user = User.find(params[:id])
-  end
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update]
+
   def new
     @user = User.new
   end
+
+  def show
+    # @user は `set_user` メソッドでセットされます
+  end
+
+  def edit
+    # @user は `set_user` メソッドでセットされます
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the MICROPOSTS!" # 成功メッセージをフラッシュする
+      flash[:success] = 'Welcome to the MICROPOSTS!' # 成功メッセージをフラッシュする
       redirect_to @user # ユーザー詳細ページにリダイレクト
     else
       # バリデーションエラー時の処理
       render :new, status: :unprocessable_entity # :unprocessable_entity ステータスを設定することで、Turboがエラーを適切に処理できるようにする
     end
   end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = 'Profile updated'
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :area, :password, :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,18 @@
 class User < ApplicationRecord
-  #データ保存時に小文字にする
-  before_save { self.email = self.email.downcase } 
-  #入力必須かつ50文字以上入力要求
+  # データ保存時に小文字にする
+  before_save { self.email = email.downcase }
+  # 入力必須かつ50文字以上入力要求
   validates :name,
-    presence: true,
-    length: { maximum: 50 } 
-  #アドレスの正規表現パターンを定義
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i 
-  #アドレスのルール：
-  ##入力必須で255文字上限、正規表現とマッチ、一意となるように
+            presence: true,
+            length: { maximum: 50 }
+  # アドレスの正規表現パターンを定義
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  # アドレスのルール：
+  # #入力必須で255文字上限、正規表現とマッチ、一意となるように
   validates :email,
-    presence: true, 
-    length: { maximum: 255 },
-    format: { with: VALID_EMAIL_REGEX },
-    uniqueness: { case_sensitive: false } 
+            presence: true,
+            length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
   has_secure_password
 end
